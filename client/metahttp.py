726d56333ee46b6c1fa32cbb432269e4d97c898c
Bjørnar Grip Fjær <bjornar.fjar@gmail.com>
2010-08-13 13:03:20 +0200

    Fixed issue with non-existent variable

--- client/metahttp.py
+++ client/metahttp.py
@@ -80,7 +80,7 @@ class HTTPSAuthServerConnection(httplib.HTTPSConnection):
         self.ca_certs = ca_certs
     def connect(self):
         sock = socket.create_connection((self.host, self.port), self.timeout)
-        if self._tunnel_host:
+        if hasattr(self, "_tunnel_host") and self._tunnel_host:
             self.sock = sock
             self._tunnel()
         self.sock = ssl.wrap_socket(sock, keyfile=self.key_file, 
